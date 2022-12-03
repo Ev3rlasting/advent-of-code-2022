84b7558cff8030b340b6fa0f36b68729ea638f7c
Ray Pan <Ev3rlasting@users.noreply.github.com>
2022-12-03 13:28:12 +0800

    Create day3.py

---
+++ day3.py
@@ -0,0 +1,19 @@
+from collections import *
+a = []
+for line in open('input').readlines():
+    a.append(line.strip())
+ret = 0
+n = 0
+g = []
+for line in a:
+    g.append(line)
+    n += 1
+    if n == 3:
+        ch = set(g[0]).intersection(set(g[1])).intersection(set(g[2])).pop()
+        if ch.islower():
+            ret += ord(ch) - 96
+        else:
+            ret += ord(ch) - 38
+        g.clear()
+        n = 0
+print(ret)
