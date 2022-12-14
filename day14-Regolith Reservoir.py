8ccb1b6a782b4902080bd3add0256d5900e0e1c1
Ray Pan <Ev3rlasting@users.noreply.github.com>
2022-12-14 14:16:42 +0800

    Create day14-Regolith Reservoir.py

---
+++ day14-Regolith Reservoir.py
@@ -0,0 +1,70 @@
+import functools
+import itertools
+import math
+from collections import defaultdict, Counter
+from functools import cmp_to_key
+from pprint import pprint
+
+f = open('input')
+lines = f.readlines()
+lines = [_.strip() for _ in lines]
+minY = 500
+maxY = 0
+maxX = 0
+
+for line in lines:
+    points = line.split(' -> ')
+    temp = []
+    for point in points:
+        y, x = list(map(int, point.split(',')))
+        maxX = max(maxX, x)
+        maxY = max(maxY, y)
+        minY = min(minY, y)
+print(minY, maxY, maxX)
+
+mat = [["." for _ in range(maxY - minY + 1)] for _ in range(maxX + 1)]
+
+for line in lines:
+    points = line.split(' -> ')
+    temp = []
+    for point in points:
+        y, x = list(map(int, point.split(',')))
+        y = y - minY
+        temp.append((x, y))
+    for i in range(len(temp) - 1):
+        start, end = sorted([temp[i], temp[i + 1]])
+        for i, j in itertools.product(range(start[0], end[0] + 1),
+                                      range(start[1], end[1] + 1)):
+            mat[i][j] = '#'
+# pprint(mat)
+START = (0, 500 - minY)
+
+O = set()
+
+
+def decide(i, j):
+    if i < 0 or i == maxY or j < 0 or j == maxX - minY:
+        return False
+    down = (i + 1, j)
+    downLeft = (i + 1, j - 1)
+    downRight = (i + 1, j + 1)
+    if mat[down[0]][down[1]] == '.':
+        return decide(i + 1, j)  # move down
+    if mat[downLeft[0]][downLeft[1]] == '.':
+        return decide(i + 1, j - 1)
+    if mat[downRight[0]][downRight[1]] == '.':
+        return decide(i + 1, j + 1)
+    else:  # all three slots in the bottom line is filled, fill current slot
+        mat[i][j] = 'o'
+        O.add((i, j))
+        return True
+
+
+ret = 0
+for i in range(1000):
+    x, y = START
+    rrr = decide(x, y)
+    print(len(O))
+    ret += 1
+pprint(mat)
+# print(ret)
