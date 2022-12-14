926b6b588bee431dd065f01f41fe7ff3917714d9
Ray Pan <Ev3rlasting@users.noreply.github.com>
2022-12-14 15:06:44 +0800

    Update day14-Regolith Reservoir.py

--- day14-Regolith Reservoir.py
+++ day14-Regolith Reservoir.py
@@ -8,62 +8,46 @@ from pprint import pprint
 f = open('input')
 lines = f.readlines()
 lines = [_.strip() for _ in lines]
-minY = 500
-maxY = 0
 maxX = 0
 
+ROCK = set()
 for line in lines:
     points = line.split(' -> ')
     temp = []
     for point in points:
         y, x = list(map(int, point.split(',')))
+        y = y
         maxX = max(maxX, x)
-        maxY = max(maxY, y)
-        minY = min(minY, y)
-print(minY, maxY, maxX)
-
-mat = [["." for _ in range(maxY - minY + 1)] for _ in range(maxX + 1)]
-
-for line in lines:
-    points = line.split(' -> ')
-    temp = []
-    for point in points:
-        y, x = list(map(int, point.split(',')))
-        y = y - minY
         temp.append((x, y))
     for i in range(len(temp) - 1):
         start, end = sorted([temp[i], temp[i + 1]])
         for i, j in itertools.product(range(start[0], end[0] + 1),
                                       range(start[1], end[1] + 1)):
-            mat[i][j] = '#'
-# pprint(mat)
-START = (0, 500 - minY)
+            ROCK.add((i, j))
 
-O = set()
-
-
+START = (0, 500)
+ret = 0
+INF = maxX + 1
 def decide(i, j):
-    if i < 0 or i == maxX or j < 0 or j == maxY - minY:
-        return False
-    if mat[i + 1][j] == '.':
+    global ret
+    if (i + 1, j) not in ROCK and i < INF:
         return decide(i + 1, j)  # move down
-    if mat[i + 1][j - 1] == '.':
+    if (i + 1, j - 1) not in ROCK and i < INF:  # move left
         return decide(i + 1, j - 1)
-    if mat[i + 1][j + 1] == '.':
+    if (i + 1, j + 1) not in ROCK and i < INF:  # move right
         return decide(i + 1, j + 1)
+    if i + 1 == INF:  # reach the inf wall
+        ROCK.add((i, j))
+        ret += 1
+        return True
     else:  # all three slots in the bottom line is filled, fill current slot
-        mat[i][j] = 'o'
-        O.add((i, j))
+        ROCK.add((i, j))
+        ret += 1
         return True
 
-
-ret = 0
-for i in range(1000):
+for i in range(10000000):
     x, y = START
     rrr = decide(x, y)
-    if not rrr:
-        print(len(O))
+    if START in ROCK:
+        print(ret)
         break
-    ret += 1
-# pprint(mat)
-# print(ret)
