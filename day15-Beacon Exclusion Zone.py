043f8792e03d11f90111fd4638115912d66037d3
Ray Pan <Ev3rlasting@users.noreply.github.com>
2022-12-15 20:30:57 +0800

    Update day15-Beacon Exclusion Zone.py

--- day15-Beacon Exclusion Zone.py
+++ day15-Beacon Exclusion Zone.py
@@ -18,14 +18,10 @@ def dist(x1, y1, x2, y2):
 D = dict()
 S = []
 B = set()
-minX = float('inf')
-maxX = -minX
 for line in lines:
     line = line.replace('Sensor at x=', '').replace(': closest beacon is at ', ', ').replace('x=', '').replace('y=', '')
     sx, sy, bx, by = list(map(int, line.split(', ')))
     d = dist(sx, sy, bx, by)
-    minX = min(minX, sx, bx)
-    maxX = max(maxX, sx, bx)
     D[(sx, sy)] = d
     S.append((sx, sy))
     B.add((bx, by))
@@ -36,16 +32,13 @@ directions = ((-1, -1), (-1, 1), (1, 1), (1, -1))
 for sx, sy in S:
     d = D[(sx, sy)] + 1
     start = [sx + d, sy]
-    edges = []
     for aa, bb in directions:
         for _ in range(d):
             start[0] += aa
             start[1] += bb
             if (start[0], start[1]) in B or start[0] < 0 or start[0] > YY or start[1] < 0 or start[1] > YY:
                 continue
-            edges.append(tuple(start))
-
-    for x, y in edges:  # check if the point on edge is in no-zone of other beacons
-        if all([dist(x, y, xx, yy) > D[(xx, yy)] for xx, yy in S]):
-            print(x * YY + y)
-            sys.exit(0)
+            # check if the point on edge is in no-zone of other beacons
+            if all([dist(start[0], start[1], xx, yy) > D[(xx, yy)] for xx, yy in S]):
+                print(start[0] * YY + start[1])
+                sys.exit(0)
