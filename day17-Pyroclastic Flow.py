e88ac48c9c66014d8d951360ac54fbb68f5576e6
Ray Pan <Ev3rlasting@users.noreply.github.com>
2022-12-17 23:47:01 +0800

    Update day17-Pyroclastic Flow.py

--- day17-Pyroclastic Flow.py
+++ day17-Pyroclastic Flow.py
@@ -11,42 +11,87 @@ f = open('input')
 lines = f.readlines()
 lines = [_.strip() for _ in lines]
 
-OPS = lines[0]
+MOVES = lines[0]
 ORDER = ['-', '+', '⅃', '|', '#']
 # x,y offset to left-bottom point of the rectangle box where the shape is in
 SHAPE = {
-    '-': ((0, 0), (1, 0), (2, 0), (3, 0)),
-    '+': ((0, -1), (1, -2), (1, -1), (2, -1), (1, 0)),
-    '⅃': ((0, 0), (1, 0), (2, 0), (2, -1), (2, -2)),
-    '|': ((0, 0), (0, -1), (0, -2), (0, -3)),
-    '#': ((0, 0), (0, -1), (1, -1), (1, 0)),
+    '-': ((0 + 0j), (1 + 0j), (2 + 0j), (3 + 0j)),
+    '+': ((0 + 1j), (1 + 2j), (1 + 1j), (2 + 1j), (1 + 0j)),
+    '⅃': ((0 + 0j), (1 + 0j), (2 + 0j), (2 + 1j), (2 + 2j)),
+    '|': ((0 + 0j), (0 + 1j), (0 + 2j), (0 + 3j)),
+    '#': ((0 + 0j), (0 + 1j), (1 + 1j), (1 + 0j)),
 }
 
-# x offset of the starting point of the left-bottom rectangle box
-START = {
-    '-': (2, 0),
-    '+': (2, 2),
-    '⅃': (2, 2),
-    '|': (2, 3),
-    '#': (2, 1),
+DIR = {
+    '>': (1 + 0j),
+    '<': (-1 + 0j),
 }
 
-DROPPED = {}
+
+def getDots(coord, shape):
+    return [dotOffset + coord for dotOffset in SHAPE[shape]]
+
+
+# coord of the left-bottom point of shape box
+def canMove(coord, shape):
+    for dot in getDots(coord, shape):
+        if dot not in DROPPED and 7 > int(dot.real) >= 0 and int(dot.imag) >= 0:
+            continue
+        else:
+            return False
+    return True
+
+
+def fill(coord, shape):
+    h = 0
+    for dot in getDots(coord, shape):
+        DROPPED.add(dot)
+        h = max(h, int(dot.imag) + 1)
+    return h
+
+
+def draw(highest, point, shape):
+    dots = getDots(point, shape)
+    mat = [['.' for _ in range(7)] for _ in range(highest + 4)]
+    for dot in dots:
+        mat[~int(dot.imag)][int(dot.real)] = '@'
+    for dot in DROPPED:
+        mat[~int(dot.imag)][int(dot.real)] = '#'
+    started = False
+    for line in mat:
+        if '@' in line: started = True
+        if started:
+            print(''.join(line))
+
+
+DROPPED = set()
 
 highest = 0
 n = 0
-move = 0
-while True:
-    for s in ORDER:
-        if n == 2023:
-            print(highest)
-            break
-        start_x = START[s]
-        start_y = highest + 3
-
-        print(f'Shape {s} starting at {(start_x, start_y)}')
-        # shape = SHAPE[s]
-        # direction = OPS[move % len(OPS)]
-        n += 1
-
-# print(ret)
+block = 0
+while block < 2024:
+    for shape in ORDER:
+        block += 1
+        prev = highest
+        if block == 2023:
+            print('part1', highest)
+            sys.exit(0)
+        point = complex(2, highest + 3)
+        print(f'Shape "{shape}" starting at {point}, current stack height: {highest}')
+        # draw(highest + 3, point, shape)
+        while True:
+            direction = DIR[MOVES[n % len(MOVES)]]
+            nextJetLocation = point + direction
+            if canMove(nextJetLocation, shape):
+                point = nextJetLocation
+            if canMove(point - 1j, shape):
+                point = point - 1j
+            else:
+                highest = max(highest, fill(point, shape))
+                n += 1
+                # print(f'\tcurrent point at {point}')
+                # draw(highest + 3, point, shape)
+                break
+            n += 1
+            # print(f'\tcurrent point at {point}')
+            # draw(highest + 3, point, shape)
