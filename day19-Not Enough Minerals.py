dd7c8848a344b0520d6dc70624eb32c7f417ec3c
Ray Pan <Ev3rlasting@users.noreply.github.com>
2022-12-20 22:16:59 +0800

    Update day19-Not Enough Minerals.py

--- day19-Not Enough Minerals.py
+++ day19-Not Enough Minerals.py
@@ -13,7 +13,7 @@ f = open('input')
 lines = f.readlines()
 lines = [_.strip() for _ in lines]
 
-ret = 0
+
 BLUEPRINTS = []
 for line in lines:
     line = line.replace('Blueprint ', '').replace('Each ore robot costs ', '') \
@@ -28,61 +28,64 @@ for line in lines:
     c4 = list(map(int, (config[3].replace('ore and', ',').replace(' obsidian', '').split(','))))
     # print(c1, c2, c3, c4)
     BLUEPRINTS.append(((c1, 0, 0), (c2, 0, 0), (c3[0], c3[1], 0), (c4[0], 0, c4[1])))
-pprint(BLUEPRINTS)
+# pprint(BLUEPRINTS)
 
-# @functools.lru_cache(maxsize=None)
-MIN = 24
-
+MIN = 32
 
 @functools.lru_cache(maxsize=None)
 def dfs(r0, r1, r2, r3,
         b0, b1, b2,
         m0, m1, m2,
-        blueprint, minute):
-    # if minute == MIN - 1 - blueprint[3][2] and b2 == 0:
-    #     return 0
+        best_sofar, blueprint, minute):
     # print(f'min {minute}\t r0 {r0}\t r1 {r1}\t r2 {r2}\t r3 {r3}\t b0 {b0}\t b1 {b1}\t b2 {b2}\t b3 {b3}')
     if minute == MIN: return r3
     p0 = p1 = p2 = p3 = p4 = 0
-    # build geode robot (b3)
     minute += 1
+    t = MIN - minute
+    # assume from now on you have *infinite* resource and start to build geodes bot every future minute,
+    # can it beat the best_sofar? If not, stop trying
+    if t * (t + 1) // 2 + r3 <= best_sofar: return r3
+    # build geode robot (b3)
     if r2 >= blueprint[3][2] and r0 >= blueprint[3][0]:
-        p0 = dfs(r0 - blueprint[3][0] + b0, r1 + b1, r2 - blueprint[3][2] + b2, r3 + MIN - minute,
-                 b0, b1, b2,
-                 m0, m1, m2,
-                 blueprint, minute)
+        # always build geode robot, dont try other options (not sure if it works for all inputs but ok for me)
+        return dfs(r0 - blueprint[3][0] + b0, r1 + b1, r2 - blueprint[3][2] + b2, r3 + t,
+                   b0, b1, b2,
+                   m0, m1, m2,
+                   max(best_sofar, r3 + t), blueprint, minute)
+
     # build obsidian robot (b2)
-    if r1 >= blueprint[2][1] and r0 >= blueprint[2][0] and b2 < m2:
+    if r1 >= blueprint[2][1] and r0 >= blueprint[2][0] and b2 < m2 and b2 * t + r2 < m2 * t:
         p1 = dfs(r0 - blueprint[2][0] + b0, r1 - blueprint[2][1] + b1, r2 + b2, r3,
                  b0, b1, b2 + 1,
                  m0, m1, m2,
-                 blueprint, minute)
+                 best_sofar, blueprint, minute)
     # build clay robot (b1)
-    if r0 >= blueprint[1][0] and b1 < m1:
+    if r0 >= blueprint[1][0] and b1 < m1 and b1 * t + r1 < m1 * t:
         p2 = dfs(r0 - blueprint[1][0] + b0, r1 + b1, r2 + b2, r3,
                  b0, b1 + 1, b2,
                  m0, m1, m2,
-                 blueprint, minute)
+                 best_sofar, blueprint, minute)
     # build ore robot (b0)
-    if r0 >= blueprint[0][0] and b0 < m0:
+    if r0 >= blueprint[0][0] and b0 < m0 and b0 * t + r0 < m0 * t:
         p3 = dfs(r0 - blueprint[0][0] + b0, r1 + b1, r2 + b2, r3,
                  b0 + 1, b1, b2,
                  m0, m1, m2,
-                 blueprint, minute)
+                 best_sofar, blueprint, minute)
     p4 = dfs(r0 + b0, r1 + b1, r2 + b2, r3,
              b0, b1, b2,
              m0, m1, m2,
-             blueprint, minute)
+             best_sofar, blueprint, minute)
     return max(p0, p1, p2, p3, p4)
 
-
-for idx, blueprint in enumerate(BLUEPRINTS[:]):
+ret = 1
+for idx, blueprint in enumerate(BLUEPRINTS[:3]):
     idx += 1
     m0 = max([blueprint[_][0] for _ in range(4)])
     m1 = max([blueprint[_][1] for _ in range(4)])
     m2 = max([blueprint[_][2] for _ in range(4)])
-    print(m0, m1, m2)
-    score = dfs(2, 0, 0, 0, 1, 0, 0, m0, m1, m2, blueprint, 2)
-    ret += score * idx
+    # print(m0, m1, m2)
+    score = dfs(2, 0, 0, 0, 1, 0, 0, m0, m1, m2, 0, blueprint, 2)
+    # print(score)
+    ret *= score
     gc.collect()
 print(ret)
