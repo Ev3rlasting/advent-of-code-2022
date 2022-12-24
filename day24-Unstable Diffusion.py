9c81cf0067c1f84feea5218cc791e7bf81bf9faa
Ray Pan <Ev3rlasting@users.noreply.github.com>
2022-12-24 14:14:09 +0800

    Create day24-Unstable Diffusion.py

---
+++ day24-Unstable Diffusion.py
@@ -0,0 +1,135 @@
+import collections
+import functools
+import heapq
+import itertools
+import math
+import sys
+from collections import defaultdict, Counter
+import functools
+from pprint import pprint
+from copy import deepcopy
+
+f = open('input')
+lines = f.readlines()
+lines = [_.strip() for _ in lines]
+WALL = set()
+SNOW = defaultdict(list)
+ROAD = {}
+for j, line in enumerate(lines):
+    for i, ch in enumerate(line):
+        if ch == '#':
+            WALL.add((i, j))
+        elif ch in '><^v':
+            SNOW[(i, j)].append(ch)
+        else:
+            ROAD[(i, j)] = ch
+N = len(lines[0])
+M = len(lines)
+start = (1, 0)
+end = (N - 2, M - 1)
+
+print('end', end)
+
+
+def adj(i, j):
+    for a, b in ((i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)):
+        if N > a >= 0 and M > b >= 0:
+            yield a, b
+
+
+ret = float('inf')
+
+
+@functools.lru_cache(maxsize=None)
+def snowMove(minute):
+    snow = deepcopy(SNOW)
+    # if minute == 1:
+    #     print(snow)
+    while minute:
+        newSnow = defaultdict(list)
+        for i, j in snow:
+            for ch in snow[(i, j)]:
+                ii, jj = i, j
+                if ch == '>':
+                    ii += 1
+                    if (ii, jj) in WALL:
+                        ii = 1
+                elif ch == '<':
+                    ii -= 1
+                    if (ii, jj) in WALL:
+                        ii = N - 2
+                elif ch == '^':
+                    jj -= 1
+                    if (ii, jj) in WALL:
+                        jj = M - 1
+                        if (ii, jj) in WALL:
+                            jj -= 1
+                elif ch == 'v':
+                    jj += 1
+                    if (ii, jj) in WALL:
+                        jj = 0
+                        if (ii, jj) in WALL:
+                            jj += 1
+                newSnow[(ii, jj)].append(ch)
+        snow = newSnow
+        minute -= 1
+    # road = set((i, j) for i, j in itertools.product(range(N), range(M)) if (i, j) not in WALL and (i, j) not in newSnow)
+    return newSnow
+
+
+ret = float('inf')
+score = dict()
+q = [(0, start)]
+
+# pprint(SNOW)
+visited = set()
+
+
+def dfs(i, j, minute, path):
+    global ret
+    if minute > 100: return
+    print(i, j, minute)
+    path.append((i, j))
+    if (i, j) == end:
+        ret = min(ret, minute)
+        print(path)
+        return
+    minute += 1
+    snow = snowMove(minute)
+
+    for a, b in adj(i, j):
+        if (a, b, minute) not in visited and (a, b) not in snow and (a, b) not in WALL:
+            dfs(a, b, minute, path[:])
+            visited.add((a, b, minute))
+    if (i, j) in snow: return
+    if (i, j, minute) not in visited:
+        dfs(i, j, minute, path[:])
+        visited.add((i, j, minute))
+
+
+dfs(1, 0, 0, [])
+print(ret)
+#
+# while q:
+#     print(q)
+#     minute, (i, j) = heapq.heappop(q)
+#     if (i, j) == end:
+#         print('part1', minute)
+#         break
+#     minute += 1
+#     road, snow = snowMove(minute)
+#     # print(f'At {minute} minute, \n\troad is {road}\n\tnewSnow is {snow}')
+#     for a, b in adj(i, j):
+#         if (a, b) in road: #and score.get((a, b), float('inf')) > minute
+#             heapq.heappush(q, (minute, (a, b)))
+#             # score[(a, b)] = minute
+#     heapq.heappush(q, (minute, (i, j)))
+# score[(i, j)] = minute
+# def backtrack(i, j, minute, snow):
+#     global ret
+#     if (i, j) == end:
+#         ret = min(ret, minute)
+#         return minute
+#     road, newSnow = snowMove(minute, snow)
+#     print(f'At {minute} minute, road is {pprint(road)}\n, newSnow is {pprint(newSnow)}')
+#     for
