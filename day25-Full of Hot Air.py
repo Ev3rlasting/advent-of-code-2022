bb884c829c9afabf7627cc3845b6b0837f864f9d
Ray Pan <Ev3rlasting@users.noreply.github.com>
2022-12-26 22:22:17 +0800

    Update day25-Full of Hot Air.py

--- day25-Full of Hot Air.py
+++ day25-Full of Hot Air.py
@@ -1,50 +1,21 @@
 f = open('input')
 lines = f.readlines()
 lines = [_.strip() for _ in lines]
-MAP = {
-    '=': -2,
-    '-': -1
-}
+total = 0
+for line in lines:
+    n = 0
+    for ch in line:
+        n *= 5
+        n += int(ch) if ch.isnumeric() else -" -=".index(ch)
+    total += n
+print('total', total)
 
-RULE = {
-    3: (1, '='),
-    4: (1, '-'),
-    5: (1, 0),
-    -1: (0, '-'),
-    -2: (0, '='),
-    -3: (-1, 2),
-    -4: (-1, 1),
-    -5: (-1, 0)
-}
-
-
-def add(a, b):
-    short, long = (a, b) if len(a) <= len(b) else (b, a)
-    short = short[::-1]
-    long = long[::-1]
-    i = 0
-    s = []
-    carry = 0
-    while i < len(long):
-        if i < len(short):
-            summ = int(MAP.get(short[i], short[i])) + int(MAP.get(long[i], long[i])) + carry
-        else:
-            summ = int(MAP.get(long[i], long[i])) + carry
-        if 0 <= summ <= 2:
-            carry, digit = 0, summ
-        else:
-            carry, digit = RULE[summ]
-        s.append(digit)
-        i += 1
-    if carry != 0:
-        if carry > 0:
-            s.append(carry)
-        elif carry == -1:
-            s.append('=')
-        else:
-            s.append('-')
-    return ''.join(list(map(str, s))[::-1])
-ret = lines[0]
-for line in lines[1:]:
-    ret = add(ret, line)
-print('part1', ret)
+ret = ''
+while total:
+    total, m = divmod(total, 5)
+    if m <= 2:
+        ret += str(m)
+    else:
+        ret += "   =-"[m]
+        total += 1
+print(ret[::-1])
